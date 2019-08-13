d1b77bbba82987d80ced54442c3ed3c2d07f7251
icgeass <icgeass@hotmail.com>
2019-08-13 09:06:21 +0800

    微调

--- src/main/java/org/apache/ibatis/reflection/invoker/package-info.java
+++ src/main/java/org/apache/ibatis/reflection/invoker/package-info.java
@@ -13,7 +13,4 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-/**
- * Unused.
- */
 package org.apache.ibatis.reflection.invoker;
